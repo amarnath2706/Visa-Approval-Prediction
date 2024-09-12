c24358ebd1994e25be00730ef7e79e90a3ac7481
Amar <amarnath.arjsd@gmail.com>
2024-09-12 12:58:46 +0530

    commit

--- demo.py
+++ demo.py
@@ -1,6 +1,12 @@
 from visa_approval.logger import logging
 
+from visa_approval.exception import visaException
+import sys
 
 
 logging.info('This is our custom log.')
 
+try:
+    a=2/0
+except Exception as e:
+    raise visaException(e,sys)
