e4a06613528d2295869233c6c61af831e74e814c
Amar <amarnath.arjsd@gmail.com>
2024-09-19 19:42:50 +0530

    commit

--- visa_approval/constants/__init__.py
+++ visa_approval/constants/__init__.py
@@ -1,18 +1,18 @@
 import os
 from datetime import date
 
-DATABASE_NAME = "US_VISA"
+DATABASE_NAME = "visa_approval"
 
 COLLECTION_NAME = "visa_data"
 
 MONGODB_URL_KEY = "MONGODB_URL"
 
-PIPELINE_NAME: str = "usvisa"
+PIPELINE_NAME: str = "visa_approval"
 ARTIFACT_DIR: str = "artifact"
 
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
-FILE_NAME: str = "usvisa.csv"
+FILE_NAME: str = "visa_approval.csv"
 MODEL_FILE_NAME = "model.pkl"
