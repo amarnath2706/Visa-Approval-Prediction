4feefd33e7af199e33b128487cbad8519ee0056b
Amar <amarnath.arjsd@gmail.com>
2024-09-20 17:40:43 +0530

    commit

--- visa_approval/constants/__init__.py
+++ visa_approval/constants/__init__.py
@@ -18,8 +18,8 @@ FILE_NAME: str = "visa_approval.csv"
 MODEL_FILE_NAME = "model.pkl"
 
 #Data ingestion related constants
-DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
-DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_COLLECTION_NAME: str = "DATA_VISA"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion" #it will Create the folder inside the artifact directory
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
