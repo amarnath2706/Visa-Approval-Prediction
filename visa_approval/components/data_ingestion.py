462278b962908438f01aa4432484b3f9268c7282
Amar <amarnath.arjsd@gmail.com>
2024-09-21 11:47:41 +0530

    commit

--- visa_approval/components/data_ingestion.py
+++ visa_approval/components/data_ingestion.py
@@ -51,3 +51,31 @@ def export_data_into_feature_store(self)->DataFrame:
 
         except Exception as e:
             raise visaException(e,sys)
+        
+# Method to perform Train and Test split operation
+def split_data_as_train_test(self,dataframe: DataFrame) ->None:
+        """
+        Method Name :   split_data_as_train_test
+        Description :   This method splits the dataframe into train set and test set based on split ratio 
+        
+        Output      :   Folder is created in s3 bucket
+        On Failure  :   Write an exception log and then raise an exception
+        """
+        logging.info("Entered split_data_as_train_test method of Data_Ingestion class")
+
+        try:
+            train_set, test_set = train_test_split(dataframe, test_size=self.data_ingestion_config.train_test_split_ratio)
+            logging.info("Performed train test split on the dataframe")
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            
+            logging.info(f"Exporting train and test file path.")
+            train_set.to_csv(self.data_ingestion_config.training_file_path,index=False,header=True)
+            test_set.to_csv(self.data_ingestion_config.testing_file_path,index=False,header=True)
+
+            logging.info(f"Exported train and test file path.")
+        except Exception as e:
+            raise visaException(e, sys) from e
