c6d138783f46c105a52af065bbd34235e802e790
Amar <amarnath.arjsd@gmail.com>
2024-09-21 11:06:09 +0530

    commit

--- demo.py
+++ demo.py
@@ -1,12 +1,17 @@
-from visa_approval.logger import logging
+#from visa_approval.logger import logging
 
-from visa_approval.exception import visaException
-import sys
+#from visa_approval.exception import visaException
+#import sys
 
 
-logging.info('This is our custom log.')
+#logging.info('This is our custom log.')
 
-try:
-    a=2/0
-except Exception as e:
-    raise visaException(e,sys)
+#try:
+    #a=2/0
+#except Exception as e:
+    #raise visaException(e,sys)
+
+import os
+
+mongo_db_url = os.getenv('MONGODB_URL')
+print(mongo_db_url)
