1daa0faa26ed031e7dd4d84ab8a732c35bef83f0
Amar <amarnath.arjsd@gmail.com>
2024-09-21 07:17:50 +0530

    commit

--- visa_approval/entity/config_entity.py
+++ visa_approval/entity/config_entity.py
@@ -5,3 +5,19 @@ from datetime import datetime
 
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
+"""Decorator for data class - without self keyword iam writing the variables
+in our our normal class we define something like class abc:
+def __init__(self):
+self.test = "None"
+"""
+
+#With the help of data class decorator we can define the class variables without using any kind of self method
+
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
