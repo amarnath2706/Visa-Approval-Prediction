a3300447cddc608ae1857c01376975127539b816
Amar <amarnath.arjsd@gmail.com>
2024-09-22 13:25:44 +0530

    commit

--- visa_approval/constants/__init__.py
+++ visa_approval/constants/__init__.py
@@ -17,9 +17,22 @@ FILE_NAME: str = "visa_approval.csv"
 #Model name
 MODEL_FILE_NAME = "model.pkl"
 
+#validation
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
 #Data ingestion related constants
 DATA_INGESTION_COLLECTION_NAME: str = "DATA_VISA"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion" #it will Create the folder inside the artifact directory
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store" #it will Create the folder inside the artifact directory
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2 #train-test split ratio
+
+
+#Data validation related constants
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
