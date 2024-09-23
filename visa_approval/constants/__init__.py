876da7cd4bcc893a572bd6b60999d0af56786fa4
Amar <amarnath.arjsd@gmail.com>
2024-09-23 19:04:06 +0530

    commit

--- visa_approval/constants/__init__.py
+++ visa_approval/constants/__init__.py
@@ -38,9 +38,16 @@ DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report" #it will Create the folde
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 #Data transformation related constants
-"""
-Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
-"""
+
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation" #it will Create the folder inside the artifact directory
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+#Model Trainer constants
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer" 
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+
