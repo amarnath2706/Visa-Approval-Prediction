978fbfd36da2ef942ee939922909e089e89079fc
Amar <amarnath.arjsd@gmail.com>
2024-09-23 07:41:53 +0530

    commit

--- visa_approval/pipline/training_pipeline.py
+++ visa_approval/pipline/training_pipeline.py
@@ -16,6 +16,7 @@ from visa_approval.entity.artifact_entity import (DataIngestionArtifact,DataVali
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validataion_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -35,6 +36,31 @@ class TrainPipeline:
             raise visaException(e, sys) from e
         
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise visaException(e, sys) from e
+        
+
 
 
     def run_pipeline(self, ) -> None:
@@ -46,3 +72,5 @@ class TrainPipeline:
 
         except Exception as e:
             raise visaException(e, sys)
+
+    
