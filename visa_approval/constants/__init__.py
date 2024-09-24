50557eed6863e671110467da9d2b5891016c3698
Amar <amarnath.arjsd@gmail.com>
2024-09-24 22:45:17 +0530

    commit

--- visa_approval/constants/__init__.py
+++ visa_approval/constants/__init__.py
@@ -56,5 +56,12 @@ MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6 #How much score we expect from our model
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
+#Model Evaluation constants
+
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model2024"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
 
 
